68d891c8cb616479477232070b40c54cc74fcfb7
ARod <ajr.rodriguez095@gmail.com>
2022-02-02 19:07:29 -0500

    inital logic

--- Develop/logic.js
+++ Develop/logic.js
@@ -3,19 +3,31 @@
 var displayCurrentDay = function() {
   var currentTimeEl = $("#currentDay");
 
-
     var date = new Date();
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
     currentTimeEl.text(new Intl.DateTimeFormat("en-US",options).format(date)); 
 };
 
-// get tasks id and value and store in object
+
 
 
 // save tasks to local storage
 var saveTasks = function(){
 
+  // get tasks id and value and store in object
+  var taskId = $().val();
+  var taskDescription = $().val();
+
+  // create object to store variables
+  var tasks = {
+    id:"",
+    description:""
+  }
+
 };
+
+// event listener for save task button
+
 
 
 
