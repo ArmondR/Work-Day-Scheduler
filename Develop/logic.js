9169fc760ad0bb0fef346251e0bf3d5560b596b9
ARod <ajr.rodriguez095@gmail.com>
2022-02-05 09:07:50 -0500

    time block audit

--- Develop/logic.js
+++ Develop/logic.js
@@ -1,6 +1,8 @@
 var saveBtnEl = $(".saveBtn");
 var descriptionEl = $(".description");
-var timeBlockEl = $(".hour");
+var timeBlockEl = $(".time-block");
+
+
 
 var displayCurrentDay = function() {
   var currentTimeEl = $("#currentDay");
@@ -10,60 +12,83 @@ var displayCurrentDay = function() {
     currentTimeEl.text(new Intl.DateTimeFormat("en-US",options).format(date)); 
 };
 
+var currentTime = function() {
+  var options = {hour: 'numeric', hour12: false};
+  var time = (new Intl.DateTimeFormat("en-US",options).format()); 
+  return time;
+}
+
+// establish time variable for use with task time audit 
+var time = currentTime();
 
 
 
 // save tasks to local storage
 var saveTasks = function(){
 
-  
+  // gets value of description from div in which button was clicked
   var taskDescription = $(this).siblings(".description").val();
   console.log(taskDescription);
 
+  // gets id of parent container in which button was clicked
   var taskId = $(this).parent().attr("id");
   console.log(taskId);
 
+  // stores values recieved in an object
   var tasks = {
     id: taskId,
     description: taskDescription
   }
-
   console.log(tasks);
 
-  // if(event.target.matches(".saveBtn")) {
-  //   console.log(".description".value);
-  //}
-  
-  //console.log(event.target);
+  // recieves values from local storage, however if there isn't an values we create an empty array
+  var userTask = JSON.parse(localStorage.getItem("userTask")||"[]");
 
-  // // get tasks id and value and store in object
-  // var taskId = $()
-  // var taskDescription = $().val();
+  // adds values to array in local storage
+  userTask.push(tasks);
 
-  // // create object to store variables/ key values
-  // var tasks = {
-  //   id:taskId,
-  //   description:taskDescription
-  // }
+  // saves values in usertask array to local storage
+  localStorage.setItem("userTask",JSON.stringify(userTask));
 
-  // localstorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTasks = function () {
+ localStorage.getItem("userTask");
+};
+
+
+var taskAudit = function() {
+
+  // loop through each task div
+  $(".time-block").each(function() {
+
+    // if class is present remove it.
+    //$(this).removeClass("present past future");
+
+    var taskTime = parseInt($(this).attr("data-time"));
+   console.log(taskTime);
+  // update classes according to their time 
+    if (time == taskTime) {
+      $(this).addClass("present");
+    }
+    else if (time > taskTime) {
+      $(this).addClass("past");
+    }
+    else if (time < taskTime) {
+      $(this).addClass("future");
+    }
+  })
 };
 
 
 
 
-
-
-
 displayCurrentDay();
+currentTime();
+loadTasks();
+taskAudit();
 
 // Event listener/delegation for save button
-//$(".container").click(saveTasks);
 $(".container").on("click", "button", saveTasks);
 
-// event listener for save task button
-// $(".container").on("click", function(event) {
-//   alert(event.target.saveBtnEl);
 
-//   console.log(event);
-// })
